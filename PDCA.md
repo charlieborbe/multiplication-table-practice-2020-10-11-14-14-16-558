2e35877e5461f705e6704edf52405592c3435eda
BORBECH <charlieborbs18@gmail.com>
2020-10-14 01:05:36 +0800

    Multiplication Table - Programming with Context Map and PDCA

---
+++ PDCA.md
@@ -0,0 +1,34 @@
+###P - plan methods in creating program (context map methods)
+###D - implement methods created in context map
+###C - Issues/Blockers encounter during DO phase
+###A - Actions or Improvements needed to be done in order to reduce the operation and prevent blockers
+
+### printMultiplicationTable
+#### P - 1
+#### D - 1
+#### C - 
+#### A - 
+
+### isValidInputNumber
+#### P - 2
+#### D - 2
+#### C - 
+#### A -
+
+### isLessThanOrEqualToEndNumber
+#### P - 5
+#### D - 5
+#### C - 
+#### A - 
+
+### isInputNumberInRange
+#### P - 5
+#### D - 5
+#### C - 
+#### A - 
+
+### generateMultiplicationTable
+#### P - 10
+#### D - 30
+#### C - Change my approach to StringBuilder to String, having problem with format and spaces
+#### A - Next I will look carefully at the expected output and test it carefully
--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -1,7 +1,45 @@
 package com.tw;
 
 public class MultiplicationTable {
-    public String create(int startNumber, int endNumber) {
+    public String printMultiplicationTable(int startNumber, int endNumber) {
+        boolean isValid = isValidInputNumber(startNumber, endNumber);
+        String multiplicationTable = generateMultiplicationTable(startNumber, endNumber);
+
+        return isValid ? multiplicationTable : null;
+    }
+
+    private String generateMultiplicationTable(int startNumber, int endNumber) {
+        if(isValidInputNumber(startNumber,endNumber)) {
+            String multiplicationTable = new String();
+            for (int counter = startNumber; counter <= endNumber; counter++) {
+                for (int multiplyCount = startNumber; multiplyCount <= counter; multiplyCount++) {
+                    multiplicationTable += multiplyCount + ("*") + (counter) + ("=") + (counter * multiplyCount);
+                    if (counter == multiplyCount) {
+                        multiplicationTable += "";
+                    } else {
+                        multiplicationTable += "  ";
+                    }
+                }
+                multiplicationTable += "\r\n";
+            }
+            return multiplicationTable.trim();
+        }
         return null;
+    }
+
+
+    private boolean isValidInputNumber(int startNumber, int endNumber) {
+        boolean isStartNumberInRange = isInputNumberInRange(startNumber, endNumber);
+        boolean isEndNumberBiggerThanStartNumber = isLessThanOrEqualToEndNumber(startNumber, endNumber);
+        return isStartNumberInRange && isEndNumberBiggerThanStartNumber;
+    }
+
+    private boolean isLessThanOrEqualToEndNumber(int startNumber, int endNumber) {
+        return startNumber <= endNumber;
+    }
+
+    private boolean isInputNumberInRange(int startNumber, int endNumber) {
+        boolean result = startNumber <= 100 && startNumber >= 1 && endNumber <= 100 && endNumber >= 1;
+        return result;
     }
 }
